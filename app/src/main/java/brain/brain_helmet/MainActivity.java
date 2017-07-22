706a647b174f79cddbb691b05ca1cec185a46935
nalgharabawi <nada.algharabawi@gmail.com>
2017-07-22 11:46:12 -0400

    My First commit

---
+++ app/src/main/java/brain/brain_helmet/MainActivity.java
@@ -0,0 +1,13 @@
+package brain.brain_helmet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
